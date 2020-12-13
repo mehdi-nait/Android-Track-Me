57e704a28ef78565601bd865415b0bdfe202155e
Mehdi NAIT HAMMOU <mehdi.nait.hammou1@gmail.com>
2020-12-13 14:43:19 +0100

    first commit

---
+++ ScanmeV1/app/src/main/java/com/ensim/scanmev1/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.ensim.scanmev1.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.ensim.scanmev1.Articles;
+import com.ensim.scanmev1.Home;
+import com.ensim.scanmev1.R;
+import com.ensim.scanmev1.Scan;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.frag_home, R.string.frag_scan,R.string.frag_articles};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch(position){
+            case 0:
+                fragment = new Home();
+                break;
+            case 1:
+                fragment = new Scan();
+                break;
+            case 2:
+                fragment = new Articles();
+                break;
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 3;
+    }
+}
